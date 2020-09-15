cd739dec44d079a685b47fd2d1fdf73f48114fb0
waylandsingh <wmsingh@ucdavis.edu>
2020-09-14 20:46:51 -0700

    started pseudocode for hash tables ransom note

--- hash_tables_ransom_note/w2_solution.py
+++ hash_tables_ransom_note/w2_solution.py
@@ -5,10 +5,21 @@ import os
 import random
 import re
 import sys
+from collections import counter
 
 # Complete the checkMagazine function below.
 def checkMagazine(magazine, note):
-
+	# naive go-round: counter stores # occurences of each word in mag
+	mag_dict = Counter()
+	
+	# counter stores # occur of each word needed for note
+	
+	#compare the two hash tables to see if all note.keys are in mag.keys
+	
+	#compare #occur for each word and return True if all magnum>notenum
+	
+	
+	
 if __name__ == '__main__':
     mn = input().split()
 
