0b53a90ffae1d7de6e4348a4751a8d4ccba45b81
waylandsingh <wmsingh@ucdavis.edu>
2020-09-15 20:39:04 -0700

    completed hash tables question

--- hash_tables_ransom_note/w2_solution.py
+++ hash_tables_ransom_note/w2_solution.py
@@ -5,20 +5,24 @@ import os
 import random
 import re
 import sys
-from collections import counter
+from collections import Counter
 
 # Complete the checkMagazine function below.
 def checkMagazine(magazine, note):
-	# naive go-round: counter stores # occurences of each word in mag
-	mag_dict = Counter()
-	
-	# counter stores # occur of each word needed for note
-	
-	#compare the two hash tables to see if all note.keys are in mag.keys
-	
-	#compare #occur for each word and return True if all magnum>notenum
-	
-	
+    # naive go-round: counter stores # occurences of each word in mag
+    mag_count = Counter(magazine)
+    
+    # counter stores # occur of each word needed for note
+    note_count = Counter(note)
+    
+    #compare the two hash tables to see if all note.keys are in mag.keys
+    #"dumb" way with direct comparison and iteration
+    for word in note_count:
+        if not (word in mag_count.keys() and note_count[word]<=mag_count[word]):
+            return 'No'
+    #compare #occur for each word and return True if all magnum>notenum
+    
+    return 'Yes'
 	
 if __name__ == '__main__':
     mn = input().split()
