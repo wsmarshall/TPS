2a07f80f1b6a292cc72c8219de47eec4789f43d7
waylandsingh <wmsingh@ucdavis.edu>
2020-09-21 20:43:52 -0700

    minimal change to quartiles.py

--- ten_day_stats/quartiles.py
+++ ten_day_stats/quartiles.py
@@ -2,5 +2,9 @@
 # quartiles
 
 # get input from stdin
+from sys import stdin
+lines = stdin.readlines()[1:]
 
 # define function to run to get the quartiles (use numpy
+def calculate_quartiles(nums):
+	pass
